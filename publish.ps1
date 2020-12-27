2ee602ab4f435158b57edd59030e89913a78ed7d
nightblade9 <nightbladecodes@gmail.com>
2020-12-27 11:40:14 -0500

    Proper readme and publication script

---
+++ publish.ps1
@@ -0,0 +1,38 @@
+$ErrorActionPreference = 'Stop'
+$gameName = "EasyModeStore"
+$version = "v1.0.0"
+$requiredFiles = @('manifest.json', 'shops.json', 'readme.txt')
+
+################################################################
+
+$publishDir = [IO.Path]::Combine($pwd, "publish")
+
+write-host "Version $version"
+
+$zipsPath = [IO.Path]::Combine(".", "*.zip")
+remove-item $zipsPath
+
+$zipFile = "$gameName-$version.zip"
+
+if (Test-Path($publishDir))
+{
+    Remove-Item $publishDir -Recurse
+}
+
+New-Item -Path $publishDir -ItemType Directory
+
+# Copy all content over
+foreach ($file in $requiredFiles)
+{
+    Copy-Item $file $publishDir
+}
+
+# Zip it up
+if (Test-Path($zipFile))
+{
+    Remove-Item $zipFile -Force
+}
+
+Add-Type -A 'System.IO.Compression.FileSystem'
+[IO.Compression.ZipFile]::CreateFromDirectory($publishDir, $zipFile);
+Write-Host DONE! Zipped to $zipFile
---
+++ readme.txt
@@ -0,0 +1,21 @@
+[Easy Mode Store]
+
+Easy-mode store mod for Stardew Valley. Adds difficult-to-get things to Pierre's store, at affordable prices.
+
+[Requirements]
+
+Requires the Shop Tile Framework mod (https://www.nexusmods.com/stardewvalley/mods/5005) and all transitive dependencies / related mods (Json Assets, etc.)
+
+[Installation]
+
+Simply download and unzip the mod to your `Mods` directory and you're good to go
+
+[Customization]
+
+If you wish to add your own items to the store, open up "shops.json". Simply copy/paste the second-last item to make a new item. The fields are self-explanatory: 
+
+- ItemNames: specifies the in-game name of the item (in square brackets)
+- StockPrice: the price (in gold) for the item
+- Stock: how many of that item Pierre has, per-day
+
+If you're interested in more complex configuration, check the Shop Tile Framework readme.
